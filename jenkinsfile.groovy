23b676270b391c188cb084eaaa8e18f83c1003cd
Dinesh Kumaran <d,kumaran.m@gmail.com>
2021-08-29 18:22:38 +0530

    0002 added parameters, updated arguments in shell scripts

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,6 +1,11 @@
 pipeline {
     agent any
 
+    parameters{
+        string(name: 'FirstName',Description: 'Enter your first name')
+        string(name: 'SecondName',Description: 'Enter your second name')
+    }
+
 
     stages {
         stage("echo"){
@@ -9,7 +14,7 @@ pipeline {
                     echo "Going to run a shell script:"
                     sh """
                     chmod +x testshell.sh
-                    ./testshell.sh Dinesh Kumaran"""
+                    ./testshell.sh "${params.FirstName}" "${params.SecondName}" """
                 }
 
             }
